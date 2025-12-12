9f759413148381d0bb1e6dce2635cda3c8a9f894
Sedima Mohale <sedimamohale18@gmail.com>
2025-12-12 11:29:02 +0200

    integrate search, filter, sort, and pagination logic into App.jsx

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import PodcastGrid from "./components/PodcastGrid";
+import { genres } from "./data";
+import { fetchPodcasts } from "./api/fetchPodcasts";
+import Header from "./components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ * App - The root component of the Podcast Explorer application. It handles:
+ * - Fetching podcast data from a remote API
+ * - Managing loading and error states
+ * - Rendering the podcast grid once data is successfully fetched
+ * - Displaying a header and fallback UI during loading or error
+ * @returns {JSX.Element} The rendered application interface
+ */
+export default function App() {
+  const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchPodcasts(setPodcasts, setError, setLoading);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Header />
+      <main>
+        {loading && (
+          <div className="message-container">
+            <div className="spinner"></div>
+            <p>Loading podcasts...</p>
+          </div>
+        )}
 
-export default App
+        {error && (
+          <div className="message-container">
+            <div className="error">
+              Error occurred while tyring fetching podcasts: {error}
+            </div>
+          </div>
+        )}
+
+        {!loading && !error && (
+          <PodcastGrid podcasts={podcasts} genres={genres} />
+        )}
+      </main>
+    </>
+  );
+}
